3a70350cbcd63ec9350da67569eaa93e49a6ffb8
bbgeneralplus2028-star <bbgeneralplus2028@gmail.com>
2026-05-24 19:12:57 -0400

    Create equity_agent.py

---
+++ agents/equity_agent.py
@@ -0,0 +1,7 @@
+class EquityAgent:
+    def calculate(self, property_data):
+        return {
+            "estimated_value": 320000,
+            "estimated_equity": 145000,
+            "arv": 345000
+        }
