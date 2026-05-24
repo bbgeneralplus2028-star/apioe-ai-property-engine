97780f2ae75fc2aec019965b7f249fdc44d0bd88
bbgeneralplus2028-star <bbgeneralplus2028@gmail.com>
2026-05-24 19:06:55 -0400

    Create legal_agent.py

---
+++ agents/legal_agent.py
@@ -0,0 +1,7 @@
+class LegalAgent:
+    def scan(self, property_data):
+        return {
+            "foreclosure_risk": "LOW",
+            "tax_liens": False,
+            "judgments": False
+        }
