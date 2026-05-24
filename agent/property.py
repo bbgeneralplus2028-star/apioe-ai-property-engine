62a1055318ea8b1cab942e885acbfd6f5f9a313a
bbgeneralplus2028-star <bbgeneralplus2028@gmail.com>
2026-05-24 19:04:29 -0400

    Create property.py

---
+++ agent/property.py
@@ -0,0 +1,8 @@
+class PropertyAgent:
+    def fetch(self, address):
+        return {
+            "address": address,
+            "apn": "36-01202 01-00031",
+            "type": "Single Family",
+            "year_built": 1998
+        }
